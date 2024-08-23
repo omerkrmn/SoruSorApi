4c9894cb79c1ce975e981243e9a3bbd5fa8571a0
siyahgezegen <92273328+siyahgezegen@users.noreply.github.com>
2024-08-23 04:02:48 +0300

    Added Login Page

---
+++ Frontend/Frontend/Frontend/Pages/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Frontend.Pages
+{
+
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtUserName.Focus();
+        }
+
+        private void txtUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUserName.Text) && txtUserName.Text.Length>0)
+            {
+                textEmail.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textEmail.Visibility = Visibility.Visible;
+            }
+
+        }
+    }
+}
